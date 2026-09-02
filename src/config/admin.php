0b384ceaca0de768214712f26d8056211aba5f86
Roberto Enrico <robertoenrico_@hotmail.com>
2026-09-02 12:12:50 -0300

    Melhorias na segurança ao fazer reset de senha admin

--- src/config/admin.php
+++ src/config/admin.php
@@ -2,5 +2,6 @@
 
 return [
     'name' => env('ADMIN_NAME', 'Administrador'),
-    'password' => env('ADMIN_PASSWORD', 'change-me-now'),
+    'password' => env('ADMIN_PASSWORD'),
+    'reset_password' => env('ADMIN_RESET_PASSWORD', false),
 ];
